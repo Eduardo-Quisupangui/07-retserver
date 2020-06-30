3f174c3fca16678527eb65cdc8ed79dddf6b5290
Eduardo-Quisupangui <qsanty_666@hotmail.es>
2020-06-30 11:11:56 -0500

    ac2

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,4 @@
+//configurar el puerto
+//en la maquina virtual va a dar el 3000
+//en heroku nos da un pueto  
 process.env.PORT = process.env.PORT || 3000;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') //utilizamos
 
+//esta parte de codigo es un midelword que trabaja en una capa intermedia
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -14,7 +15,7 @@ app.get('/usuario', (req, res) => {
 
 app.post('/usuario', (req, res) => {
     let body = req.body
-
+        //se hace una validacion para pida mecesariamente el nombre o salga un error  400 
     if (body.nombre === undefined) {
         res.status(400).json({
             ok: false,
@@ -41,10 +42,13 @@ app.delete('/usuario', (req, res) => {
 
 
 app.listen(process.env.PORT, () => {
-        console.log("Escuchando en el puerto", process.env.PORT);
-    })
-    //antes que viaje del cervidor como http
-    //los datos de la web tiene que estar codificada 
-    //body cuerpo de la peticion 
-    //miderworld una capa intermedia 
-    //en el post se izo una modifcacion y le mada a imprimir
+    console.log("Escuchando en el puerto", process.env.PORT);
+});
+//antes que viaje del cervidor como http
+//los datos de la web tiene que estar codificada 
+//body cuerpo de la peticion 
+//miderworld una capa intermedia 
+//en el post se izo una modifcacion y le mada a imprimir
+
+//para installar body colocamos
+//npm install body-parser --save
